87b1a40a5cb8ac0002c4a77c3637ed9fb77bfd33
Sachith Muhandiram <sachith@vizuamatix.com>
2021-12-04 22:41:24 +0530

    feat: Initial setup was set for DoublyLinkedList project.

--- build/test/CTestTestfile.cmake
+++ build/test/CTestTestfile.cmake
@@ -4,4 +4,4 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(LinkedListTest "/home/sachith/Documents/Personal/Projects/datastructures/build/test/LinkedListTest")
+add_test(DoublyLinkedListTest "/home/sachith/Documents/Personal/Projects/datastructures/build/test/DoublyLinkedListTest")
